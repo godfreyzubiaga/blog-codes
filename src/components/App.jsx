9543434758b0baac53d408efef83c770f2d013b4
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2019-02-11 17:15:07 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import Counter from './Counter';
 
 const StyledDiv = styled.div`
 	text-align: center;
 `;
 
-const App = () => (
-  <StyledDiv>
-    <Counter />
-  </StyledDiv>
-);
+const App = () => <StyledDiv>Hello World</StyledDiv>;
 
 export default App;
